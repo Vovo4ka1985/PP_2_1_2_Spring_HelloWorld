dfaf3a02df9de92ef6edac57beae5c8139857f5a
Vovo4ka1985 <hammett2010@mail.ru>
2022-05-14 19:50:48 +0400

    new

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+       // System.out.println(bean.getMessage());
+       // System.out.println(bean1.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+       // System.out.println(bean2.getName());
+       // System.out.println(bean3.getName());
+
+        boolean equalsRes = bean == bean1;
+        boolean equalsRes1 = bean2 == bean3;
+
+        System.out.println(equalsRes);
+        System.out.println(equalsRes1);
     }
 }
